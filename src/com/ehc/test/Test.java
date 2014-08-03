9ee04893a4fdfa61bdc52c95058ae11dadcbf531
keshavchinna <y.keshav@rocketmail.com>
2014-08-03 23:12:47 +0530

    getting latest commits and branches corresponding Repositories

--- src/com/ehc/test/Test.java
+++ src/com/ehc/test/Test.java
@@ -3,7 +3,7 @@ package com.ehc.test;
 import org.kohsuke.github.*;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,44 +15,64 @@ import java.util.Set;
  * To change this template use File | Settings | File Templates.
  */
 public class Test {
-  public static String OAuthToken = "bc985cb63621207b3168779917278b82e9e6685c";
+    public static String OAuthToken = "bc985cb63621207b3168779917278b82e9e6685c";
 
-  public static void main(String args[]) {
-    try {
-      GitHub hub = GitHub.connectUsingOAuth(OAuthToken);
-      GHRepository repo = hub.getRepository("preym/OAuth");
-      GHOrganization organization = hub.getOrganization("ehc");
-      Map<String, GHTeam> teams = organization.getTeams();
-      Iterator entries = teams.entrySet().iterator();
-      while (entries.hasNext()) {
-        Map.Entry entry = (Map.Entry) entries.next();
-        String key = (String) entry.getKey();
-        GHTeam value = (GHTeam) entry.getValue();
-        Set<GHUser> users = value.getMembers();
-        System.out.println("Key = " + key + ", Value = " + value);
-        System.out.println("Developers/Team Members:" + users.toString());
-      }
-      Map<String, GHBranch> branches = repo.getBranches();
-      Map<String, GHRepository> repositories = organization.getRepositories();
-      Iterator entries1 = repositories.entrySet().iterator();
-      while (entries.hasNext()) {
-        Map.Entry entry = (Map.Entry) entries.next();
-        String key = (String) entry.getKey();
-        GHRepository value = (GHRepository) entry.getValue();
-        Map<String, GHBranch> users = value.getBranches();
-        System.out.println("Key = " + key + ", Value = " + value);
-        System.out.println("Developers/Team Members:" + users.toString());
-      }
-      System.out.println("Details: " + repo.toString());
-      System.out.println("organization: " + organization.toString());
-      System.out.println("branches: " + branches.toString());
-      System.out.println("Teams: " + teams.toString());
-      System.out.println("Repositories: " + repositories.toString());
-    } catch (IOException e) {
-      System.out.println("Exception: " + e.getCause());
-      System.out.println("Exception: " + e.getMessage());
-      e.printStackTrace();
+    public static void main(String args[]) {
+        String sha1 = "";
+        try {
+            GitHub hub = GitHub.connectUsingOAuth(OAuthToken);
+            GHOrganization organization = hub.getOrganization("ehc");
+            Map<String, GHTeam> teams = organization.getTeams();
+            System.out.println("--------------------------");
+            for (Map.Entry<String, GHTeam> entry : teams.entrySet()) {
+                System.out.println(entry.getKey() + "/" + entry.getValue());
+                Set<GHUser> users = entry.getValue().getMembers();
+                for (GHUser user : users) {
+                    System.out.println("TeamMembers: " + user);
+                }
+            }
+            System.out.println("--------------------------");
+
+            Map<String, GHRepository> repositories = organization.getRepositories();
+            for (Map.Entry<String, GHRepository> repositoryEntry : repositories.entrySet()) {
+                System.out.println("--------------------------");
+                System.out.println(repositoryEntry.getKey() + " =/= " + repositoryEntry.getValue());
+                System.out.println("Repository: " + repositoryEntry.getKey());
+
+                for (String names : repositoryEntry.getValue().getCollaboratorNames()) {
+                    System.out.println("Name: " + names);
+                }
+                for (GHRef ref : repositoryEntry.getValue().getRefs()) {
+                    String branch = ref.getRef();
+
+                    System.out.println("branch: " + branch);
+
+                    String sha = ref.getObject().getSha();
+                    System.out.println("sha: " + sha);
+
+
+                    for (Map.Entry<String, GHBranch> branchEntry : repositoryEntry.getValue().getBranches().entrySet()) {
+                        System.out.println(branchEntry.getKey() + "=/=" + branchEntry.getValue());
+                        sha1 = branchEntry.getValue().getSHA1();
+                        System.out.println("sha1: " + sha1);
+                    }
+
+                    GHCommit commit = repositoryEntry.getValue().getCommit(sha1);
+
+                    System.out.println("commit: " + commit.toString());
+                    System.out.println("author: " + commit.getAuthor().toString());
+                    if (commit.getCommitter().toString() != null)
+                        System.out.println("Committer: " + commit.getCommitter().toString());
+                }
+            }
+            System.out.println("--------------------------");
+
+
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getCause());
+            System.out.println("Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
-
-  }
 }
